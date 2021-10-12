2ac2aec95430c752aa0b1704b63fe31aad80bfe4
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-12 06:44:59 +0300

    publish message with NATS

--- publisher.js
+++ publisher.js
@@ -1,9 +1,15 @@
 const nats = require('node-nats-streaming');
+const crypto = require('crypto');
 
 const stan = nats.connect('ticketing','abc', { url: 'http://localhost:4222' });
 
 stan.on('connect', ()=>{
-    stan.publish('foo', 'Hello from the other side', (err, guid)=>{
+    const ticketData = JSON.stringify({
+        id: crypto.randomUUID().toString(),
+        title: 'K8s Event 1',
+        price: 2500
+    })
+    stan.publish('ticket:created', ticketData, (err, guid)=>{
         if (err) {
             console.log('publish failed: ' + err)
           } else {
