811c2700c475fbd26076d231a53c9056b1b68716
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-13 07:20:48 +0300

    process data using manual ack mode

--- listener.js
+++ listener.js
@@ -8,10 +8,11 @@ const stan = nats.connect('ticketing', crypto.randomUUID().toString() , {
 });
 
 stan.on('connect', ()=>{
-    const listenOpts = stan.subscriptionOptions().setStartWithLastReceived();
+    const listenOpts = stan.subscriptionOptions().setStartWithLastReceived().setManualAckMode(true);
     const subscribe = stan.subscribe('ticket:created','orders-service-queue-group', listenOpts);
     subscribe.on('message', (msg)=>{
         console.log(`Message received: [ ${msg.getSequence()} ]`);
         console.log(`Message received: [ ${msg.getData()} ]`);
+        msg.ack();
     })
 })
