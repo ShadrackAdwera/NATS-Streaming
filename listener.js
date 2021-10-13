806627501bb1b081d287c07586352c351396bfe8
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-13 07:44:53 +0300

    graceful client shut down

--- listener.js
+++ listener.js
@@ -10,9 +10,20 @@ const stan = nats.connect('ticketing', crypto.randomUUID().toString() , {
 stan.on('connect', ()=>{
     const listenOpts = stan.subscriptionOptions().setStartWithLastReceived().setManualAckMode(true);
     const subscribe = stan.subscribe('ticket:created','orders-service-queue-group', listenOpts);
+
+    stan.on('close', () => {
+        console.log('NATS connection closed');
+        process.exit();
+    })
+
     subscribe.on('message', (msg)=>{
         console.log(`Message received: [ ${msg.getSequence()} ]`);
         console.log(`Message received: [ ${msg.getData()} ]`);
         msg.ack();
     })
 })
+
+//interrupt signal
+process.on('SIGINT', ()=> stan.close());
+//terminate signal
+process.on('SIGTERM', ()=> stan.close());
