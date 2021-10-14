b509e06a42972dfb4571175b41f6afe3cf740eb2
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-14 11:29:22 +0300

    create a subscription on the listener class

--- listener.js
+++ listener.js
@@ -1,50 +1,66 @@
-const crypto = require('crypto');
-const nats = require('node-nats-streaming');
+const crypto = require("crypto");
+const nats = require("node-nats-streaming");
 
 console.clear();
 
-const stan = nats.connect('ticketing', crypto.randomUUID().toString() , {
-    url: 'http://localhost:4222' 
+const stan = nats.connect("ticketing", crypto.randomUUID().toString(), {
+  url: "http://localhost:4222",
 });
 
-stan.on('connect', ()=>{
-    const listenOpts = stan
+stan.on("connect", () => {
+  const listenOpts = stan
     .subscriptionOptions()
     .setManualAckMode(true)
     .setDeliverAllAvailable()
-    .setDurableName('accounts-service');
-    const subscribe = stan.subscribe('ticket:created','orders-service-queue-group', listenOpts);
+    .setDurableName("accounts-service");
+  const subscribe = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    listenOpts
+  );
 
-    stan.on('close', () => {
-        console.log('NATS connection closed');
-        process.exit();
-    })
+  stan.on("close", () => {
+    console.log("NATS connection closed");
+    process.exit();
+  });
 
-    subscribe.on('message', (msg)=>{
-        console.log(`Message received: [ ${msg.getSequence()} ]`);
-        console.log(msg.getData());
-        msg.ack();
-    })
-})
+  subscribe.on("message", (msg) => {
+    console.log(`Message received: [ ${msg.getSequence()} ]`);
+    console.log(msg.getData());
+    msg.ack();
+  });
+});
 
 //interrupt signal
-process.on('SIGINT', ()=> stan.close());
+process.on("SIGINT", () => stan.close());
 //terminate signal
-process.on('SIGTERM', ()=> stan.close());
+process.on("SIGTERM", () => stan.close());
 
 class Listener {
-    constructor(subject, queueGroupName, client, ackWait) {
-        this.subject = subject;
-        this.queueGroupName = queueGroupName;
-        this.client = client,
-        this.ackWait = ackWait; //5*1000
-    }
-    subscriptionOptions() {
-        return this.client
-        .subscriptionOptions()
-        .setDeliverAllAvailable()
-        .setManualAckMode(true)
-        .setAckWait(this.ackWait)
-        .setDurableName(this.queueGroupName)
-    }
+  constructor(subject, queueGroupName, client, ackWait) {
+    this.subject = subject;
+    this.queueGroupName = queueGroupName;
+    (this.client = client), (this.ackWait = ackWait); //5*1000
+  }
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg) => {
+      console.log(`Message received: [ ${msg.getSequence()} ]`);
+      console.log(msg.getData());
+      msg.ack();
+    });
+  }
 }
