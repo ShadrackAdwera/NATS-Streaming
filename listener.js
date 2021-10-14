4fa486146bafb890572c15b3e3de05ecb7c07296
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-14 12:51:57 +0300

    code refactor

--- listener.js
+++ listener.js
@@ -1,5 +1,6 @@
 const crypto = require("crypto");
 const nats = require("node-nats-streaming");
+const Listener = require('./events/base-listener');
 
 console.clear();
 
@@ -8,26 +9,12 @@ const stan = nats.connect("ticketing", crypto.randomUUID().toString(), {
 });
 
 stan.on("connect", () => {
-  const listenOpts = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName("accounts-service");
-  const subscribe = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group",
-    listenOpts
-  );
 
+const ticketCreatedListener = new Listener('ticket:created','payments-service', stan, 5000);
+ticketCreatedListener.listen();
   stan.on("close", () => {
     console.log("NATS connection closed");
     process.exit();
-  });
-
-  subscribe.on("message", (msg) => {
-    console.log(`Message received: [ ${msg.getSequence()} ]`);
-    console.log(msg.getData());
-    msg.ack();
   });
 });
 
@@ -36,31 +23,3 @@ process.on("SIGINT", () => stan.close());
 //terminate signal
 process.on("SIGTERM", () => stan.close());
 
-class Listener {
-  constructor(subject, queueGroupName, client, ackWait) {
-    this.subject = subject;
-    this.queueGroupName = queueGroupName;
-    (this.client = client), (this.ackWait = ackWait); //5*1000
-  }
-  subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
-  }
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
-
-    subscription.on("message", (msg) => {
-      console.log(`Message received: [ ${msg.getSequence()} ]`);
-      console.log(msg.getData());
-      msg.ack();
-    });
-  }
-}
