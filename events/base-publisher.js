3a2decc9f2dc8a1a02d42af4977ba85c67c8f55a
ShadrackAdwera <adweshshaddie@gmail.com>
2021-10-18 15:02:01 +0300

    outsource publisher code into a separate class

---
+++ events/base-publisher.js
@@ -0,0 +1,15 @@
+class Publisher {
+    constructor(subject, client) {
+        this.subject = subject;
+        this.client = client;
+    }
+    publish(data) {
+        this.client.publish(this.subject, data, (err,guid)=>{
+            if (err) {
+                console.log('publish failed: ' + err)
+              } else {
+                console.log('published message with guid: ' + guid)
+              }
+        })
+    }
+}
