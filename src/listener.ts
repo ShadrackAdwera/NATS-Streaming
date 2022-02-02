9bd6d317791bc93df644ce559363bfedc0a3c8dd
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-02 15:53:47 +0300

    handle durable subscriptions and event redelivery

--- src/listener.ts
+++ src/listener.ts
@@ -15,9 +15,12 @@ stan.on('connect', ()=>{
         process.exit();
     });
 
-    const options = stan.subscriptionOptions().setManualAckMode(true);
+    const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable().setDurableName('tickets-service');
 
-    const subscription = stan.subscribe('ticket:created','orders-service-queue-group', options);
+    const subscription = stan.subscribe('ticket:created', 'queue-group-name' ,options);
 
     subscription.on('message', (msg: Message)=>{
         const data = msg.getData();
