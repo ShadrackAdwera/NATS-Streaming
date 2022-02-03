7b43f3fa48b8bf3555ee958b228a3dd4eab1ab95
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-03 12:11:42 +0300

    create subjects enum

---
+++ src/events/ticket-created-listener.ts
@@ -0,0 +1,16 @@
+import { Message } from 'node-nats-streaming';
+import { Listener } from './base-listener';
+
+export class TicketCreatedListener extends Listener {
+    subject = 'ticket:created';
+    queueGroupName = 'payments-service';
+    onMessage(data: any, msg: Message): void {
+        try {
+            console.log(`Event Data: `, data);
+            msg.ack();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+}
